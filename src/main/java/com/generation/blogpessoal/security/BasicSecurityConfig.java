e4c10b87f979629678638f780c1e657a61b0eb38
Juliana <julianarayane23@hotmail.com>
2022-07-11 09:54:01 -0300

    novas atualizações

--- src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
+++ src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
@@ -34,10 +34,17 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/usuarios/logar").permitAll().antMatchers("/usuarios/cadastrar")
-				.permitAll().antMatchers(HttpMethod.OPTIONS).permitAll().anyRequest().authenticated().and().httpBasic()
-				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and()
-				.csrf().disable();
+		http.authorizeRequests()
+		.antMatchers("/**").permitAll()
+		.antMatchers("/usuarios/logar").permitAll()
+		.antMatchers("/usuarios/cadastrar").permitAll()
+		.antMatchers(HttpMethod.OPTIONS).permitAll()
+		.anyRequest().authenticated()
+		.and().httpBasic()
+		.and().sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and().cors()
+		.and().csrf().disable();
 	}
 
 }
